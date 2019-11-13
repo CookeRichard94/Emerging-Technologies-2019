af32a9a039a746525024e5b443586ecb951eb14d
Richard Cooke <g00331787@gmit.ie>
2019-11-13 18:12:05 +0000

    Added to html page

--- web-app/web-app.py
+++ web-app/web-app.py
@@ -1,3 +1,9 @@
+# Richard Cooke
+# G00331787
+
+# Flask web app to that reads the data in the static html page and passes it to the model in the jupyter notebook
+
+# Importing flask
 import flask as fl
 
 app = fl.Flask(__name__)
